b6d921a50ec886d11d72be8c89e1117a970ca530
SpitfireMk2b-san <tysmc23@gmail.com>
2021-04-14 09:16:50 -0400

    Add to github

---
+++ dsc.py
@@ -0,0 +1,55 @@
+import discord
+from json import dumps
+from base64 import urlsafe_b64encode
+from requests import get
+import PySimpleGUI as sg
+layout = [[sg.Text("Discord Token:")],
+          [sg.Input(key='-INPUT-')],
+          [sg.Button('Ok')]]
+
+
+window = sg.Window('Enter Token', layout)
+
+_, values = window.read()
+token = values['-INPUT-'].strip().strip("\"")
+print(token)
+window.close()
+del window
+del layout
+layout = [[sg.Text("Enter Channel ID")],
+          [sg.Input(key='-INPUT-')],
+          [sg.Button('Ok')]]
+
+
+window = sg.Window('Window Title', layout)
+
+_, values = window.read()
+selectedchannelid = int(values['-INPUT-'].strip().strip("\""))
+window.close()
+del window
+del layout
+client = discord.Client()
+
+serverport = 5080
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    ch = client.get_channel(selectedchannelid)
+    chhist = []
+    async for message in ch.history(limit=20):
+        chhist.append(message)
+    for message in reversed(chhist):
+        msd = urlsafe_b64encode(
+            bytes(dumps({"uname": message.author.display_name, "content": message.clean_content}), 'utf-8')).decode(
+            "utf-8")
+        get("http://localhost:" + str(serverport) + "/" + msd)
+
+@client.event
+async def on_message(message):
+    if message.channel.id != selectedchannelid: return
+    msd = urlsafe_b64encode(bytes(dumps({"uname": message.author.display_name, "content": message.clean_content}), 'utf-8')).decode("utf-8")
+    get("http://localhost:"+str(serverport)+"/"+msd)
+
+
+client.run(token, bot=False)
---
+++ ui.py
@@ -0,0 +1,31 @@
+import webview
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from threading import Thread
+from base64 import urlsafe_b64decode
+from json import loads, dumps
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/json')
+        self.end_headers()
+        self.wfile.write(b'received')
+        dat = loads(urlsafe_b64decode(self.path.strip("/")).decode("utf-8"))
+        window.evaluate_js("create_message("+dumps(dat['uname'])+", "+dumps(dat['content'])+")")
+
+server = HTTPServer(('', 5080), MyHandler)
+Thread(target=server.serve_forever, daemon=True).start()
+
+class Api:
+    def __init__(self):
+        self.cancel_heavy_stuff_flag = False
+
+    def closebutton(self):
+        window.destroy()
+
+    def minbutton(self):
+        window.minimize()
+
+api = Api()
+window = webview.create_window('mini discord window', 'htdoc/main.html', transparent=True, js_api=api, frameless=True, easy_drag=False, on_top=True, height=200, width=300)
+webview.start(debug=True)
